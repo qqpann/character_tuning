b0e82d84be641afa87bb0459d0fbff0894e26b6c
Qiushi Pan <17402261+qqpann@users.noreply.github.com>
2023-07-23 01:20:43 +0900

    add: role prompt

--- model.py
+++ model.py
@@ -8,11 +8,20 @@ load_dotenv()
 openai.organization = os.getenv("OPENAI_ORGANIZATION")
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+ROLE_PROMPT = """
+Act as a AI VTuber and chat with your viewers.
+"""
 
+
+# TODO: Replace with your own model with your own prompt
 def chat(message: str) -> str:
     # create a chat completion
     chat_completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", messages=[{"role": "user", "content": message}]
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": ROLE_PROMPT},
+            {"role": "user", "content": message},
+        ],
     )
 
     # return the chat completion
