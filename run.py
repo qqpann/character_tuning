f2fa1b0fc0c773095981e38b5d4ca88cabfdca34
Qiushi Pan <17402261+qqpann@users.noreply.github.com>
2023-07-23 02:19:33 +0900

    add: output response to csv

--- run.py
+++ run.py
@@ -1,15 +1,31 @@
+import pandas as pd
+from tqdm import tqdm
+
 from model import chat
 
 DEBUG = True
 
 
 def main():
+    df = pd.DataFrame(columns=["question", "ai_answer", "modified_answer"])
     with open("data/aituber_question_dataset/question.txt", "r") as f:
-        for line in f.readlines():
-            print("question >", line)
-            print("ai answer >", chat(line))
+        for line in tqdm(f.readlines()):
+            line = line.strip()
+            question = line
+            ai_answer = chat(line)
+            modified_answer = ""
+            df = df._append(
+                {
+                    "question": question,
+                    "ai_answer": ai_answer,
+                    "modified_answer": modified_answer,
+                },
+                ignore_index=True,
+            )
+
             if DEBUG:
                 break
+    df.to_csv(f"out/question_answer.csv", index=False)
 
 
 if __name__ == "__main__":
