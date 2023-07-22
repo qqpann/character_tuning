f00ed98b328a1b4dabea8fa3af9923187afeefd5
Qiushi Pan <17402261+qqpann@users.noreply.github.com>
2023-07-23 00:26:28 +0900

    chore: read questions

--- run.py
+++ run.py
@@ -8,6 +8,8 @@ load_dotenv()
 openai.organization = os.getenv("OPENAI_ORGANIZATION")
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+DEBUG = True
+
 
 def chat(message: str):
     # create a chat completion
@@ -20,11 +22,12 @@ def chat(message: str):
 
 
 def main():
-    while True:
-        prompt = input(" you > ")
-        if prompt == "q":
-            break
-        print(" ai >", chat(prompt))
+    with open("data/aituber_question_dataset/question.txt", "r") as f:
+        for line in f.readlines():
+            print("question >", line)
+            print("ai answer >", chat(line))
+            if DEBUG:
+                break
 
 
 if __name__ == "__main__":
